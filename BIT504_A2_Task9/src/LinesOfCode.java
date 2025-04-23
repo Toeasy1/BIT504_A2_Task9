dbbf1b9ab483d1ac3644d3f8fb173aa0c770a740
BIT504 <wehi42@gmail.com>
2025-04-24 10:49:23 +1200

    Making Of BIT504_A2_Task9 Repository.

---
+++ BIT504_A2_Task9/src/LinesOfCode.java
@@ -0,0 +1,10 @@
+//BIT504_A2_Sean_Wehi_5111545
+
+//Testing uploading to Git hub via Eclipse
+public class LinesOfCode {
+
+	public static void main(String[] args) {
+		System.out.println("Before Commit");
+	}
+
+}
